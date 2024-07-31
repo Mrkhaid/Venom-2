700d9e2c6c8a13ed416aec90a09199593b6028c3
Mrkhaid <k92144021@gmail.com>
2024-07-31 11:59:39 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,11 +31,11 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = true //auto view status/story
+global.autoswview = false //auto view status/story
 
 //menu type 
 //v1 is image menu, 
